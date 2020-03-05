918f580793f910c6e3a45f6c51a4ff21720067e8
Alison Felizzi <Alison.Felizzi@data61.csiro.au>
2020-03-05 21:24:12 +1100

    tools: Added markdown page doc generator
    Added 'gen_markdown_api_doc.py', a simple python script that
    takes a markdown file and generates a website doc page populated
    with the contents of the markdown file.

---
+++ tools/gen_markdown_api_doc.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python3
+import sys
+import os
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--file", dest="file", type=str,
+                    help="Markdown location (within repository)")
+parser.add_argument("-o","--output", dest="output", type=str,
+                    help="Output docsite file")
+args = parser.parse_args()
+
+base = os.path.basename(args.file)
+title = os.path.splitext(base)[0]
+output = '''---
+title: {0}
+---
+
+{{% include_absolute {1} %}}
+'''.format(title, args.file)
+with open(args.output, "w") as output_file:
+    output_file.write(output)
