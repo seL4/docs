b453be945216cafb86f1480d0019423567439fa4
Gerwin Klein <gerwin.klein@proofcraft.systems>
2025-07-15 18:29:16 +1000

    Add copy button to code block with language set
    This only works for code blocks with a declared language, because those
    are wrapped in a <div> and we need that diff to anchor the button.
    
    Putting the button into <pre> would work theoretically, but triggers a
    bug in Safari when he <pre> block is too wide (scrolling) and innerHTML
    of the button is being set (button text disappears).
    
    Signed-off-by: Gerwin Klein <gerwin.klein@proofcraft.systems>

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -25,3 +25,39 @@
     }
   }
 })();
+
+(function initCopyButtons() {
+  const codeBlocks = document.querySelectorAll('div > pre > code');
+
+  for (let codeBlock of codeBlocks) {
+    preBlock = codeBlock.parentElement;
+    divBlock = preBlock.parentElement;
+    const copyButton = document.createElement('button');
+    copyButton.setAttribute('aria-label', 'Copy code to clipboard');
+    copyButton.setAttribute('title', 'Copy code to clipboard');
+    copyButton.setAttribute('type', 'button');
+    copyButton.className = 'code-copy';
+    copyButton.innerHTML = "Copy";
+    divBlock.classList.add('relative');
+    divBlock.classList.add('group');
+    divBlock.insertBefore(copyButton, preBlock);
+
+    copyButton.addEventListener('click', function () {
+      copyToClipboard(copyButton, codeBlock)
+    });
+  }
+
+  async function copyToClipboard(button, codeBlock) {
+    const codeToCopy = codeBlock.innerText;
+    try {
+      await navigator.clipboard.writeText(codeToCopy);
+      button.innerHTML = "Copied!";
+      setTimeout(function() {
+        button.innerHTML = "Copy";
+      }, 1000);
+    }
+    catch (_) {
+      console.warn("Clipboard write failed.");
+    }
+  }
+})();
